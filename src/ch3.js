a7761bc11909fa9f04f7bdc08a555a1671da0968
Il Hwan You <ih.you@powderroom.co.kr>
2018-10-01 22:32:21 +0900

    c3-20

--- src/ch3.js
+++ src/ch3.js
@@ -1,1 +1,7 @@
-import { each } from 'underscore'
+import * as _ from 'underscore'
+
+export function filter2 (list, fn, limit = list.length, context) {
+  const res = []
+  _.find(list, (v, i, c) => fn.call(context, v, i, c) && res.push(v) === limit)
+  return res
+}
